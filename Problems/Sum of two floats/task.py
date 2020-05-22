10c45e2501e62d96486a9dc339c3652c97a5ac06
Shruti Turner <s.turner17@imperial.ac.uk>
2020-05-22 18:03:03 +0100

    Task 2 complete

---
+++ Problems/Sum of two floats/task.py
@@ -0,0 +1,3 @@
+num1 = input()
+num2 = input()
+print(float(num1) + float(num2))
---
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -0,0 +1,7 @@
+input_str = input("Enter cells: >")
+
+print("---------")
+print("| {0} {1} {2} |".format(input_str[0], input_str[1], input_str[2]))
+print("| {0} {1} {2} |".format(input_str[3], input_str[4], input_str[5]))
+print("| {0} {1} {2} |".format(input_str[6], input_str[7], input_str[8]))
+print("---------")
